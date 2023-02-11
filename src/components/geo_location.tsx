f5fc29f12a862e528b7cb55d797e9b94a029b253
christophergyman <christophergayiuman@gmail.com>
2023-02-11 16:31:04 +0000

    updated api and component positioning

--- src/components/geo_location.tsx
+++ src/components/geo_location.tsx
@@ -1,36 +1,54 @@
 import React, { useState, useEffect } from 'react';
+import { useQuery } from '@tanstack/react-query';
+
 function GeoLocation (){
 
 
     // Building API String
-    var latlong = [51.5072, 0.1267]
-    var string1 = 'https://api.openweathermap.org/data/2.5/weather?lat='
-    var string2 = '&lon=' 
-    var string3 = '&appid='
-    var apiKey = '184f57ba41848e91f08939cf49fada3d'
-    var metric = '&units=metric'
+    var latlong = [51.5072, 0.1267];
+    var string1 = 'https://api.openweathermap.org/data/2.5/weather?lat=';
+    var string2 = '&lon=';
+    var string3 = '&appid=';
+    var apiKey = '184f57ba41848e91f08939cf49fada3d';
+    var metric = '&units=metric';
     var apiCall = string1 + latlong[0] + string2 + latlong[1] + string3 + apiKey + metric;
 
-    //Calling the API
-    const callAPI = async () =>{
-        try {
-            const res = await fetch(apiCall);
-            const data = await res.json();
-            console.log(data);
 
-            //Grab info from data value
-            console.log(data.weather[0].main)
-            console.log(data.weather[0].description)
-            console.log(data.main.temp)
-            console.log(data.main.temp_min)
-            console.log(data.main.temp_max)
+    // Set state
+    const [weatherData, setWeatherData] = useState(null)
 
-        } catch (err) {
-            console.log(err);
-        }
-    };
+    // API Call
+    const callAPI = async () => {
+		try {
+			const res = await fetch(apiCall);
+            const data = (await res.json());
+            setWeatherData(data);
+		} catch (err) {
+			console.log(err);
+		}
+	};
 
-    // Return JSX 
+    //Set null:
+    if (weatherData){
+        return(
+            <div className='flex flex-col items-center'>
+
+                <div className=''>
+                    <div className='flex flex-col'>
+                        <p className='text-7xl text-center font-bold'>{weatherData.weather[0].main}</p>
+                        <p className='text-2xl text-center font-medium'>{weatherData.weather[0].description}</p>
+                    </div>
+                </div>
+
+                <div className='pt-9'>
+                    <p>{weatherData.main.temp}</p>
+                    <p>{weatherData.main.temp_min}</p>
+                    <p>{weatherData.main.temp_max}</p>
+                </div>
+
+            </div>
+        );
+    }
     return(
         <div>
             <button onClick={callAPI} className='btn'>callApi</button>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
 import NavBar from '@/components/navbar'
 import GeoLocation from '@/components/geo_location'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 export default function Home() { return (
     <div>
+
       <NavBar/>
-      <GeoLocation/>
+
+      <div className='pt-20'>
+        <GeoLocation/>
+      </div>
+
     </div>
   )
 }
