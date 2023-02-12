f8ae08c20197e172d121b4d5244de4d07194549d
christophergyman <christophergayiuman@gmail.com>
2023-02-12 18:32:19 +0000

    update GUI and push icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,17 @@
 import NavBar from '@/components/navbar'
 import GeoLocation from '@/components/geo_location'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import Display_box from '@/components/display_box'
 
 export default function Home() { return (
     <div>
-
       <NavBar/>
-
       <div className='pt-20'>
         <GeoLocation/>
+      </div>
+
+      <div className='pt-20'>
+        <Display_box/>
       </div>
 
     </div>
