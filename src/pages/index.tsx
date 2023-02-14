1a57bf6572ec32e72b5501ce122d6e682361ed87
christophergyman <christophergayiuman@gmail.com>
2023-02-14 16:18:04 +0000

    Updated positioning of all components in flex

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,9 @@ import GeoLocation from '@/components/geo_location'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 export default function Home() { return (
-    <div>
+    <div className='flex flex-col items-center'>
       <NavBar/>
-      <div className='pt-20'>
+      <div className='pt-52'>
         <GeoLocation/>
       </div>
     </div>
